04d9dbbd3b3e01223758100204236d62ca8083f2
Sphesihle Mhlongo <mhlongosihle49@gmail.com>
2024-12-04 17:24:18 +0200

    Pages content

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold">Home Page</h1>
+      <p className="mt-4">Welcome to my portfolio website!</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold">Home Page</h1>
+      <p className="mt-4">Welcome to my portfolio website!</p>
+    </div>
+  );
+};
+
+export default Projects;
