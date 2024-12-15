2cdcd51bb3ec3b961f8660ac8f7d6e0f37d54e93
Sphesihle Mhlongo <mhlongosihle49@gmail.com>
2024-12-15 12:06:32 +0200

    Project data for scalability

---
+++ src/data/projectsData.js
@@ -0,0 +1,26 @@
+// src/data/projectsData.js
+
+const projectsData = [
+    {
+      title: "Dreamers Club Website",
+      description: "A business Website created with Jekyll (Static site generator)",
+      projectLink: "https://dreamersclub.netlify.app/",
+      codeLink: "https://github.com/user/dreamers-club", // Replace with actual GitHub link
+      buttonStyles: {
+        projectButton: {
+          backgroundColor: "#1a202c", // Custom color
+          padding: "10px 20px",
+          borderRadius: "17px",
+        },
+        codeButton: {
+          backgroundColor: "#000000",
+          padding: "10px 20px",
+          borderRadius: "17px",
+        },
+      },
+    },
+    // Add more projects as objects
+  ];
+  
+  export default projectsData;
+  
