7f6a30288b6e524753ffc6cb3e6cae4cd7dcb32d
Sphesihle Mhlongo <120335426+sphesihlemhlongo@users.noreply.github.com>
2025-03-11 11:32:22 +0000

    New project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -21,6 +21,17 @@ const projectsData = [
       },
     },
 
+    {
+      title: "Verify Health Claim",
+      description: "Verify Health Claims made by Health influencers",
+      projectLink: "https://verifyhealthclaim.netlify.app/",
+      codeLink: "https://github.com/sphesihlemhlongo/easy-health.ai",
+      buttonStyles: {
+        projectButton: { backgroundColor: "#3182ce", padding: "10px 20px", borderRadius: "17px", userSelect: "none" },
+        codeButton: { backgroundColor: "#2d3748", padding: "10px 20px", borderRadius: "17px", userSelect: "none" },
+      },
+    },
+
   ];
   
   export default projectsData;
