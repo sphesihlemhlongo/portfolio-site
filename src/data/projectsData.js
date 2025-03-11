3facf0c5025a088ff1879751b72ef5eec79d5396
Sphesihle Mhlongo <120335426+sphesihlemhlongo@users.noreply.github.com>
2025-03-11 11:17:55 +0000

    Updated

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -14,7 +14,7 @@ const projectsData = [
       title: "Portfolio Website",
       description: "A portfolio website built with React.js.",
       projectLink: "https://sphesihlemhlongo.netlify.app/",
-      codeLink: "https://github.com/user/portfolio",
+      codeLink: "https://github.com/sphesihlemhlongo/portfolio-site",
       buttonStyles: {
         projectButton: { backgroundColor: "#3182ce", padding: "10px 20px", borderRadius: "17px", userSelect: "none" },
         codeButton: { backgroundColor: "#2d3748", padding: "10px 20px", borderRadius: "17px", userSelect: "none" },
