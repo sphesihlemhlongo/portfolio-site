e90d4df98d9079707c9a2d3cdf14b51d25600f5a
Sphesihle Mhlongo <mhlongosihle49@gmail.com>
2025-03-22 20:01:30 +0200

    Updated Build

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -28,7 +28,7 @@ const projectsData = [
       codeLink: "https://github.com/sphesihlemhlongo/easy-health.ai",
       buttonStyles: {
         projectButton: { backgroundColor: "#3182ce", padding: "10px 20px", borderRadius: "17px", userSelect: "none" },
-        codeButton: { backgroundColor: "#2d3748", padding: "10px 20px", borderRadius: "17px", userSelect: "none" },
+        codeButton: { backgroundColor: "#4F6180FF", padding: "10px 20px", borderRadius: "17px", userSelect: "none" },
       },
     },
 
